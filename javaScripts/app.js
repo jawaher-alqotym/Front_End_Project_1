c615ce8569a9f59a5137f9ff6b0f29c528646c80
jawaher-alqotym <jwahralqtym@gmail.com>
2022-06-26 09:11:16 +0300

    new commit

--- javaScripts/app.js
+++ javaScripts/app.js
@@ -1,5 +1,5 @@
 sound_is_playing = false;
-counter = 0;
+
 
 function play(id){
  if (!sound_is_playing){
@@ -18,34 +18,64 @@ function stop(id){
 }
 
 function add_task(string){
+
  if(string){
-    ul = document.getElementById("toDo_list");
-    li = document.createElement("li");
-    li.setAttribute("id", string.replace(/\s+/g,'')+(++counter));
-  
-
-    //append button&text to li
-    li.appendChild(document.createTextNode(string));
-
-    button = document.createElement('button')
-    button.setAttribute('type', 'submit')
-    button.setAttribute('onclick',` delet_task(${li.id})`)
-    button.innerHTML = 'Delete'
+    const task = {
+        id: Date.now(),
+        title: string,
+    }
+    localStorage.setItem(task.id, task.title);
     
-    //buttion css
-    button.style = 'animation: no-txt-color-gradient; color: white;'
+    //get all local storge
+    values = []
+    keys = Object.keys(localStorage),
+    i = keys.length;
 
-    li.appendChild(button)
-
-    ul.appendChild(li);
+    while ( i-- ) {
+     values.push( localStorage.getItem(keys[i]) );
     }
  
-  
+    
+    i = keys.length;
+    delet_all_tasks()
+    while(i--){
+      ul = document.getElementById("toDo_list");
+      li = document.createElement("li");
+      li.setAttribute("id", `${keys[i]}`);
+      //append button&text to li
+      li.appendChild(document.createTextNode(localStorage.getItem(keys[i])));
+
+      button = document.createElement('button')
+      button.setAttribute('type', 'submit')
+      button.setAttribute('onclick',`delet_task(${keys[i]})`)
+      button.innerHTML = 'Delete'
+    
+      //buttion css
+      button.style = 'animation: no-txt-color-gradient; color: white;'
+
+      li.appendChild(button)
+
+
+      ul.appendChild(li);
+    }
+     
+    
+     }
+    }
+ 
+function  delet_all_tasks(){
+  const myNode = document.getElementById("toDo_list");
+  myNode.innerHTML = '';
 }
 
-function delet_task(elem){
+
+function delet_task(id){
+
+    localStorage.removeItem(`${id}`);
     ul = document.getElementById("toDo_list");
-    ul.removeChild(document.getElementById(elem.id))
+    ul.removeChild(document.getElementById(id))
+
+   
 }
 
 
